e6d4a52b67a298958ef9c0c5f8e356d73b009575
jaroslav <rosajaro1@seznam.cz>
2023-06-04 21:00:29 +0200

    Oprava implementace

--- README.md
+++ README.md
@@ -1,1 +1,26 @@
-# psi-2
+# 2. úloha semestarání práce
+
+## Název: Topologie sítě
+
+## Zadání: Implementujte aplikaci, která automaticky zjistí topologii sítě
+
+## Popis
+- Implementujte aplikaci v programovacím jazyce Python, která automaticky zjistí
+topologii sítě, v které se nachází. Aplikace ke zjištění topologie využívá protokol
+SNMP, pomocí kterého získá ze směrovačů obsah směrovacích tabulek.
+Nejprve pomocí DHCP získá adresu výchozího směrovače, adresy dalších směrovačů
+pak rekuzivním způsobem z obsahu směrovacích tabulek jednotlivých směrovačů. Je
+nutné si uvědomit, že směrovač má zpravidla více rozhraní a je tedy identifikován
+více IP adresami.
+
+### Popis funkčnosti:
+
+- 
+
+### Implementace:
+
+- 
+
+### Spuštění
+
+- ./build/client/client <cislo_port> <obsah_zpravy>
--- main.py
+++ main.py
@@ -1,14 +1,25 @@
+import math
+
 from pysnmp.hlapi import *
 from scapy.all import *
 
 SNMP_PORT = 161  ## port pro SNMP
-ipAdEntAddr = "1.3.6.1.2.1.4.21.1.1.2"  ## oid pro ipRouteDest
+ipRouteDest = "1.3.6.1.2.1.4.21.1.1.2"  ## oid pro ipRouteDest
+ipAdEntNetMask = "1.3.6.1.2.1.4.20.1.3.1.0"  ## oid pro ipRouteDest
 ip = conf.route.route('0.0.0.0')[2]  ## default gateway zarizeni
-community = 'PSIPUB' ## Community rezetec site.
+community = 'PSIPUB'  ## Community rezetec site.
+
+
+ips = []  ## Pole pro nacteni IP adres site.
+found_ips = [] ## Nalezene IP adresy v
+
+
+def bit_count(self):
+    return bin(self).count("1")
 
 
 ## Funkce pro vytvoreni pysnmp commandu.
-def walk_remote(host, oid):
+def run_cmd(host, oid):
     return bulkCmd(SnmpEngine(),
                    CommunityData(community, mpModel=1),
                    UdpTransportTarget((host, SNMP_PORT), timeout=0.0, retries=0),
@@ -17,18 +28,25 @@ def walk_remote(host, oid):
                    ObjectType(ObjectIdentity(oid)))
 
 
-ips = [] ## Pole pro nacteni IP adres site.
+def process_subnet(ip, net_mask):
+    last_ip = ip.split('.', 3)
+    last_net_mask = net_mask.split('.', 3)
+    bits = 0
+    for j in range(4):
+        bits += bit_count(int(last_net_mask[j]) - int(last_ip[j]))
+    return bits
+
 
 try:
     print('Starting scanning...')
-    g = walk_remote(ip, ipAdEntAddr) ## Vytvoreni SNMP prikazu.
-    errorIndication, errorStatus, errorIndex, varBinds = next(g) ## Spusteni prvni SNMP prikazu.
-    temp = varBinds[0][-1].prettyPrint() ## Ziskani prvni IP adresy.
-    print('nalezene IP v prvnim nodu:')
-    while len(temp) > 1: ## Pokud SNMP vraci IP adresy.
-        ips.append(temp) ## Pridam adresu do pole.
-        print('-> ' + str(temp))
-        errorIndication, errorStatus, errorIndex, varBinds = next(g) ## Dalsi spusteni
+    g = run_cmd(ip, ipRouteDest)  ## Vytvoreni SNMP prikazu.
+    errorIndication, errorStatus, errorIndex, varBinds = next(g)  ## Spusteni prvni SNMP prikazu.
+    temp = varBinds[0][-1].prettyPrint()  ## Ziskani prvni IP adresy.
+    # print('nalezene IP v prvnim nodu:')
+    while len(temp) > 1:  ## Pokud SNMP vraci IP adresy.
+        ips.append(temp)  ## Pridam adresu do pole.
+        # print('-> ' + str(temp))
+        errorIndication, errorStatus, errorIndex, varBinds = next(g)  ## Dalsi spusteni
         ## Odchyceni chyb a jejich vypis.
         if errorIndication:
             print('errorIndication: ' + str(errorIndication) + ' (' + str(temp) + ')')
@@ -41,11 +59,10 @@ try:
             continue
         else:
             temp = varBinds[0][-1].prettyPrint()
-    print('-------------------------------')
     ## Nyni se projdou vsechny nalezene zarizeni podle IP adres.
     for i in range(len(ips)):
-        print('nalezene IP v na IP: ' + ips[i])
-        g = walk_remote(ips[i], ipAdEntAddr)
+        # print('nalezene IP v na IP: ' + ips[i])
+        g = run_cmd(ips[i], ipRouteDest)
         for j in range(4):
             errorIndication, errorStatus, errorIndex, varBinds = next(g)
             if errorIndication:
@@ -61,10 +78,17 @@ try:
                 temp = varBinds[0][-1].prettyPrint()
                 ## Vypisou se vsechny nalezene adresy.
                 while len(temp) > 1:
-                    ips.append(temp)
-                    print('--> ' + temp)
+                    if temp not in ips:
+                        ips.append(temp)
+                    # print('--> ' + temp)
                     errorIndication, errorStatus, errorIndex, varBinds = next(g)
                     temp = varBinds[0][-1].prettyPrint()
+    print('Found IPs: ')
+    for i in range(len(ips)):
+        print('-> ' + ips[i])
     print('Done scanning!')
+    for i in range(int(len(ips) / 2)):
+        print('For router on ip: ' + ips[i * 2] + ' is subnet ' + ips[i * 2] + '/'
+              + str(31 - process_subnet(ips[i * 2], ips[i * 2 + 1])))
 except StopIteration:
-    print('koncim')
+    print('Ending with error')
